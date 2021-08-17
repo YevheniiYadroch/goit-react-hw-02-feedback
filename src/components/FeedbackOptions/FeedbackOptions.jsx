c9a6c93f99e134f97373a12c654aa4ccbdde9f88
YevheniiYadroch <evheniiyadroch@gmail.com>
2021-08-17 14:13:39 +0100

    architecture

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './FeedbackOptions.css';
+
+class FeedbackOptions extends Component {
+    
+
+    render() {
+        return (
+            <div>
+                <button>Bad</button>
+                <button>Neutral</button>
+                <button>Good</button>
+            </div>
+        )
+    }
+}
+
+export default FeedbackOptions;
